f86accf92bda2a382583139b72046edece965f3c
SunrabbitDE <github@sunrabbit.de>
2015-10-24 23:29:06 +0200

    Installationsscript
    Über das Installationsskript kann der FCM per SCP auf einen beliebigen
    Knoten installiert werden

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Installation des Fast-Config-Modes auf einen Router
+# Vorraussetzung:
+# - Der Router ist über IP4 / IP6 erreichbar
+# - ssh/scp-Zugang ist konfiguriert (Vorzugsweise KEY)
+if [ "$1" == '' ]
+  then
+    echo "Bitte die IP des Routers als Parameter mit angeben"
+    exit 1
+fi
+
+echo ""
+echo "Fast-Config-Mode für GLUON"
+echo ""
+echo "Installation auf $1 läuft..."
+echo ""
+echo "Kopieren der Dateien (Webserver)..."
+scp -r ./www/ root@$1:/lib/gluon/status-page/
+echo ""
+echo "Kopieren der Dateien (System)"
+scp -r ./button/ root@$1:/etc/hotplug.d/
+echo ""
+echo "Fertig."
+echo ""
